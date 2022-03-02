4824859b0e0ed6edd4358a7b58a20f4267e456fb
Travis2445 <90297320+Travis2445@users.noreply.github.com>
2022-03-02 17:59:16 -0600

    need to add test 3.6

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
-  const { catagories = [], setCurrentCategory, currentCategory } = props;
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
-}, [currentCategory]);
+  }, [currentCategory]);
 
   return (
-    <header data-testid="header" className="flex-row px-1">
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
@@ -26,19 +26,19 @@ function Nav(props) {
               About me
             </a>
           </li>
-          <li>
+          <li className="mx-2">
             <span>Contact</span>
           </li>
-          {catagories.map((category) => (
+          {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && "navActive"
-              }`}
+                currentCategory.name === category.name && 'navActive'
+                }`}
               key={category.name}
             >
               <span
                 onClick={() => {
-                  setCurrentCategory(category);
+                  setCurrentCategory(category)
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
