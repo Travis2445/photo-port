759e7c49941ec5984a054a8c507a34dc5a0345e5
Travis2445 <90297320+Travis2445@users.noreply.github.com>
2022-03-10 14:04:55 -0600

    fixed test

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import About from "..";
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
+// after each test, leftover memory is cleaned up so that no false results occur
 afterEach(cleanup);
 
-describe("About component", () => {
-  // renders about test
-  // first test
-  it("renders", () => {
-    render(<About />);
-  });
-
-  // second test
-  it("matches a snapshot DOM node structure", () => {
-    const { asFragment } = render(<About />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+        render(<About />);
+    });
+  
+    // Second Test
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
